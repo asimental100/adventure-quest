b3f17a075d2a219047c5507b76868112985399d1
Adrian Simental <asimental100@hotmail.com>
2020-07-23 11:36:18 -0700

    finished user info page

---
+++ app.js
@@ -0,0 +1,19 @@
+// import functions and grab DOM elements
+import { makeUser } from './userUtils.js';
+
+const form = document.querySelector('form');
+// initialize state
+
+// set event listeners to update state and DOM
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const formData = new FormData(form);
+
+    const user = makeUser(formData);
+
+    console.log(user);
+
+    const stringyUser = JSON.stringify(user);
+    localStorage.setItem('USER', stringyUser);
+});
