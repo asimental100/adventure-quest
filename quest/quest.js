9c87627432ef834f2341ff04062750aae992cad8
Adrian Simental <asimental100@hotmail.com>
2020-07-23 14:18:23 -0700

    added some stuff

--- quest/quest.js
+++ quest/quest.js
@@ -1,1 +1,14 @@
 console.log('quest page goes here');
+
+import allQuests from '../data.js';
+import { findByID, renderSection } from '../quest/questUtils.js';
+
+const main = document.querySelector('main');
+
+const questId = 'dragon';
+
+const questData = findByID(allQuests, questId);
+
+const sectionEl = renderSection(questData);
+
+main.append(sectionEl);
